7ee67f2c40c2a8f5781f75e6a6d39f91a2d9d50d
crystal  villanueva <noelani.villanueva@gmail.com>
2020-10-25 23:32:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,12 @@
-# Write your code here. If you get stuck, at the very bottom of this lab we've included
+# Write defyour code here. If you get stuck, at the very bottom of this lab we've included
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
-
-
+require 'pry'
+def introduction(name)
+  name = "josh"
+  puts "Hi, my name is ${name}."
+end 
 
 
 
